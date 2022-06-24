c5c433a779690cf6d5ce3da23de7e71c6f5164ff
software-punduit <72712348+software-punduit@users.noreply.github.com>
2022-06-24 18:08:18 +0100

    First

---
+++ script.js
@@ -0,0 +1,6 @@
+let username = "Afolake Bello";
+let userheight = 1.53;
+let usercountry = "Nigeria"
+
+
+alert("Name: " +  username + "\n"  + "Height: " +  userheight + "\n" + "Country: " +  usercountry)
